765358467d6dd02c1334f88b17eda3fb78a5a2bf
KalaiarasanK <52280933+KalaiarasanK@users.noreply.github.com>
2019-10-15 11:47:08 +0530

    Update square.py

--- square.py
+++ square.py
@@ -1,5 +1,17 @@
+#input
 num = int(input())
 for i in range(num):
     for i in range(num):
     	print("*",end="  ")
     print()
+
+    
+   #output
+
+5
+
+*  *  *  *  *  
+*  *  *  *  *  
+*  *  *  *  *  
+*  *  *  *  *  
+*  *  *  *  *  
