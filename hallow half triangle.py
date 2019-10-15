2f51bcd99d5c700314333c9aae7335027f256112
KalaiarasanK <52280933+KalaiarasanK@users.noreply.github.com>
2019-10-15 15:23:15 +0530

    Update hallow half triangle.py

--- hallow half triangle.py
+++ hallow half triangle.py
@@ -18,3 +18,25 @@ for i in range(1,num+1):
 *   * 
 * * 
 * 
+
+
+num = int(input())
+for i in range(1,num+1):
+    for j in range(1,i+1):
+        if(j==1 or j==i or i==num):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()        
+    
+    
+    
+    
+    #output
+    
+    
+* 
+* * 
+*   * 
+*     * 
+* * * * * 
