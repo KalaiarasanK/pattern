f98b8d5d043cff61003c1d6a59d83381fb7a6beb
KalaiarasanK <52280933+KalaiarasanK@users.noreply.github.com>
2019-10-15 12:01:43 +0530

    Create Hallow Square.py

---
+++ Hallow Square.py
@@ -0,0 +1,22 @@
+#input
+num = int(input())
+for i in range(num):
+	for j in range(num):
+		if(i==0 or i==num-1 or j==0 or j==num-1):
+			print("*",end=" ")
+		else:
+		    print(" ",end=" ")
+	print()		
+	
+	
+	#output
+	
+	5
+	
+* * * * * 
+*       * 
+*       * 
+*       * 
+* * * * * 
+
+
