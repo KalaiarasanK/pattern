4105758f0c86acc54f25c1f50144f55d4ccba1b6
KalaiarasanK <52280933+KalaiarasanK@users.noreply.github.com>
2019-10-16 15:59:10 +0530

    Create alphabet pattern.py

---
+++ alphabet pattern.py
@@ -0,0 +1,18 @@
+val = 65
+for i in range(0, 5):
+    for j in range(0, i+1):
+        ch = chr(val)
+        print(ch, end=" ")
+        val = val + 1
+    print()
+
+
+#output
+
+
+
+A 
+B C 
+D E F 
+G H I J 
+K L M N O 
