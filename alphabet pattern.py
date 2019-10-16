d7a2e036845074112361b3418b3a1ca78bae3904
KalaiarasanK <52280933+KalaiarasanK@users.noreply.github.com>
2019-10-16 16:00:11 +0530

    Update alphabet pattern.py

--- alphabet pattern.py
+++ alphabet pattern.py
@@ -16,3 +16,21 @@ B C
 D E F 
 G H I J 
 K L M N O 
+
+
+
+
+val = 65
+for i in range(0, 5):
+    for j in range(0, i+1):
+        ch = chr(val)
+        print(ch, end=" ")
+    val = val + 1
+    print()
+    
+    
+    A 
+    B B 
+    C C C 
+    D D D D 
+    E E E E E 
