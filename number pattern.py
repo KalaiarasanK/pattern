bec6e93db3d56464e198d82bbc03e5bdfbceb24a
KalaiarasanK <52280933+KalaiarasanK@users.noreply.github.com>
2019-10-16 15:56:05 +0530

    Update number pattern.py

--- number pattern.py
+++ number pattern.py
@@ -13,3 +13,21 @@ for i in range(0, 5):
     4  5  6 
     7  8  9  10
     11 12 13 14 15
+
+    
+    
+    for i in range(0, 5):
+    n = 1
+    for j in range(0, i+1):
+    	print(n, end=" ")
+    	n = n + 1
+    print()
+    
+    
+    #output
+    
+    1 
+    1 2 
+    1 2 3 
+    1 2 3 4 
+    1 2 3 4 5 
